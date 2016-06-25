15dc6bdcb11e9f1c93f704075523576adef2fe9a
ominestre <ominestre@gmail.com>
2016-06-24 20:26:57 -0400

    Adds binding for homepage
    Expanding the login slide out and how to handle submiting the form to the back of the application

--- app/js/home.js
+++ app/js/home.js
@@ -14,5 +14,59 @@ exitApp.addEventListener('click', () => {
     ipcRenderer.send('app-exit');
 });
 
+/** @namespace home.nav **/
+(function(home, $, ipcRenderer){
+    let $cache = {};
+    
+    ipcRenderer.on('login-success', () => {
+        console.log('login success');
+    });
+    
+    ipcRenderer.on('login-failure', () => {
+        console.log('login-failure');
+    });
+    
+    let events = {
+        connectClick : function(e){
+            var $this = $(this);
+            
+            $this.toggleClass('active');
+            $cache.connectWindow.toggleClass('active');
+        },
+        
+        sandboxConnect : function(e){
+            let data = {
+                hostname : $cache.connectWindow.find('.input-hostname').val(),
+                username : $cache.connectWindow.find('.input-username').val(),
+                password : $cache.connectWindow.find('.input-password').val(),
+                staging : $cache.connectWindow.find('.input-staging').val()
+            };
+            
+            console.log(data);
+            
+            ipcRenderer.send('app-sandbox-login', data);
+        }
+    };
+    
+    function initCache(){
+        $cache.connect = $('#app-sandbox-connect');
+        $cache.connectWindow = $('#screen-sandbox-connect');
+        $cache.sandboxSubmit = $cache.connectWindow.find('#sandbox-login-submit');
+    }
+    
+    function initEvents(){
+        $cache.connect.on('click', events.connectClick);
+        $cache.sandboxSubmit.on('click', events.sandboxConnect);
+    }
+    
+    home.nav = {
+        init : function(){
+            initCache();
+            initEvents();
+        }
+    };
+}(home = home || {}, $, ipcRenderer));
 
+$(document).ready(() => {
+    home.nav.init();
 });
