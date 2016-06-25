a0e453176c8b0966884a7995a19b3a2a58eb5537
ominestre <ominestre@gmail.com>
2016-06-24 20:24:19 -0400

    Adds controller to handle sandbox login requests
    This will build the sandbox.json file

---
+++ app/controllers/sandbox-login.js
@@ -0,0 +1,27 @@
+/*jslint node: true */
+/*jshint esversion: 6 */
+'use strict';
+
+const fs = require('fs');
+
+module.exports = function(event, args){
+    var file = `${global.appRoot}/sandbox.json`;
+    
+    fs.stat(file, (err, stats) => {
+        if(process.env.NODE_ENV != 'production'){
+            console.log('Creating sandbox.json file');
+        }
+        
+        if(err === null){
+            fs.unlink(file);
+            fs.appendFile(file, JSON.stringify(args));
+            event.sender.send('login-success');
+        } else if(err.code === 'ENOENT'){
+            fs.appendFile(file, JSON.stringify(args));
+            event.sender.send('login-success');
+        } else {
+            console.error(`ERROR : Can't create sandbox.json file\nMESSAGE : ${err.message}`);
+            event.sender.send('login-failure');
+        }
+    });
+};
