90c5321efa68585e3c7466e2a193f70709ae542e
ominestre <michael.miller@joann.com>
2016-07-01 11:06:29 -0400

    Removes sandbox.json credentials for enhanced security
    There will be no more writing of secure elements to the filesystem.  Once initial login is validated this will use the values in the credentials form.  That way it is volatile and on app close will be removed.

--- app/controllers/sandbox-login.js
+++ app/controllers/sandbox-login.js
@@ -2,47 +2,25 @@
 /*jshint esversion: 6 */
 'use strict';
 
-const fs = require('fs');
 const request = require('request');
 
+/**
+    @desc Test connection to grab the temp archive from your sandbox.  This is done to validate your credentials are correct
+**/
 module.exports = function(event, args){
-    let file = `${global.appRoot}/sandbox.json`;
-    
-    fs.stat(file, (err, stats) => {
-        if(process.env.NODE_ENV != 'production'){
-            console.log('Creating sandbox.json file');
-        }
-        
-        /**
-            @desc this function will hit the logs for the specified instance.  If it gets a good response it will send
-                a login request IPC to the renderer process.  If not it will send a failure
-        **/
-        function testConnection(){
-            request({
-                url : `https://${args.hostname}/on/demandware.servlet/webdav/Sites/Temp`,
-                auth : {
-                    user: args.username,
-                    password: args.password
-                },
-                strictSSL : false
-            }, (err, res, body) => {
-                if(err) {
-                    console.error(`ERROR : error in test connection for sandbox login\n${err}`);
-                    event.sender.send('login-failure');
-                } else {
-                    event.sender.send('login-success');
-                }
-            });
-        }
-        
-        if(err === null){
-            fs.unlink(file);
-            fs.appendFile(file, JSON.stringify(args), testConnection());
-        } else if(err.code === 'ENOENT'){
-            fs.appendFile(file, JSON.stringify(args), testConnection());
-        } else {
-            console.error(`ERROR : Can't create sandbox.json file\nMESSAGE : ${err.message}`);
+    request({
+        url : `https://${args.hostname}/on/demandware.servlet/webdav/Sites/Temp`,
+        auth : {
+            user: args.username,
+            password: args.password
+        },
+        strictSSL : false
+    }, (err, res, body) => {
+        if(err) {
+            console.error(`ERROR : error in test connection for sandbox login\n${err}`);
             event.sender.send('login-failure');
+        } else {
+            event.sender.send('login-success');
         }
     });
 };
